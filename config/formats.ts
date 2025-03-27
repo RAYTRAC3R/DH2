32329e9bc7fa20493c169dd543e5c31c346d62ba
RAYTRAC3R <samipiplup@gmail.com>
2025-03-27 11:34:45 -0500

    Added Bluestar images
    also updated formats again

--- config/formats.ts
+++ config/formats.ts
@@ -4431,7 +4431,7 @@ export const Formats: FormatList = [
 		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Terastal Clause', 'Sleep Clause Mod', 'Data Mod'],
 		banlist: ['All Pokemon'],
         unbanlist: ['Firepaw', 'Fireheart', 'Firestar', 'Graykit', 'Graypaw', 'Graystripe', 'Tigerclaw-Paw', 'Tigerclaw', 'Tigerclaw-Star', 'Bluepaw', 'Bluefur', 'Bluestar'],
-		teambuilderFormat: 'National Dex',
+		teambuilderFormat: 'National Dex Ubers',
 	},
 	{
 		name: "[Gen 8] Weedmons",
